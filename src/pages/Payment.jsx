c203a84781eff6bc177e0518596b7944759e499e
AlexMoreni <alex.moreni47@gmail.com>
2023-10-17 01:36:07 -0300

    Creating function to add new address

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,16 +1,112 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import {
   Container,
   TitlePayment,
   ImgPayment,
   AddressTitle,
+  NoneAddress,
+  ButtonConfirm,
+  FormAddAddress,
+  TitleForm,
+  LabelForm,
+  InputFormAdd,
+  ButtonAddAddress,
+  ButtonHiddenForm,
 } from "./Payment.style";
 
 const Payment = () => {
+  const [address, setAddress] = useState(false);
+  const [cep, setCep] = useState("");
+  const [number, setNumber] = useState(0);
+  const [reference, setReference] = useState("");
+  const [buttonToggle, setButtonToggle] = useState("Adicionar");
+
+  const newAddress = (e) => {
+    e.preventDefault();
+
+    if (cep < 8) {
+      console.log("CEP faltando digitos");
+      return;
+    }
+
+    axios.defaults.withCredentials = true;
+
+    axios
+      .post("http://localhost:3000/burguer/address", {
+        cep,
+        number,
+        reference,
+      })
+      .then((response) => {
+        if (response.data.message === "Endereço Cadastrado!") {
+          const form = document.querySelector("#form");
+          form.classList.add("hidden");
+          setButtonToggle("Adicionar");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const toggleForm = (e) => {
+    const form = document.querySelector("#form");
+    const buttonToggle = document.querySelector("#buttonToggle");
+
+    if (buttonToggle.innerHTML === "Adicionar") {
+      setButtonToggle("Fechar");
+    } else {
+      setButtonToggle("Adicionar");
+    }
+
+    form.classList.toggle("hidden");
+  };
+
   return (
     <Container>
       <TitlePayment>Pagamento</TitlePayment>
       <ImgPayment src="../../public/qrcode.png" alt="" />
       <AddressTitle>Selecione o endereço</AddressTitle>
+      <ButtonHiddenForm
+        onClick={() => {
+          toggleForm();
+        }}
+        id="buttonToggle"
+      >
+        {buttonToggle}
+      </ButtonHiddenForm>
+      <div className="hidden" id="form">
+        <FormAddAddress onSubmit={newAddress} className="hidden">
+          <TitleForm>Adicionando endereço</TitleForm>
+          <LabelForm>
+            CEP:
+            <InputFormAdd
+              type="number"
+              placeholder="Digite o CEP"
+              onChange={(e) => setCep(e.target.value)}
+            />
+          </LabelForm>
+          <LabelForm>
+            Número:
+            <InputFormAdd
+              type="number"
+              placeholder="Digite o número"
+              onChange={(e) => setNumber(e.target.value)}
+            />
+          </LabelForm>
+          <LabelForm>
+            Referencia:
+            <InputFormAdd
+              type="text"
+              placeholder="Digite uma referencia"
+              onChange={(e) => setReference(e.target.value)}
+            />
+          </LabelForm>
+          <ButtonAddAddress type="submit" value="Adicionar" />
+        </FormAddAddress>
+      </div>
+      {!address && <NoneAddress>Nenhum endereço encontrado!</NoneAddress>}
+      <ButtonConfirm>Confirmar</ButtonConfirm>
     </Container>
   );
 };
--- src/pages/Payment.style.jsx
+++ src/pages/Payment.style.jsx
@@ -32,3 +32,103 @@ export const AddressTitle = styled.h3`
   line-height: 88.896%;
   text-transform: uppercase;
 `;
+
+export const NoneAddress = styled.h1`
+  font-family: var(--font-emphasis);
+  font-size: 3rem;
+  color: var(--color-white);
+  text-align: center;
+  text-transform: uppercase;
+  line-height: 88.896%;
+  padding-top: 80px;
+`;
+
+export const ButtonConfirm = styled.button`
+  font-family: var(--font-emphasis);
+  font-size: 3rem;
+  color: var(--color-white);
+  background-color: #3fb43d;
+  margin-top: 80px;
+  border-radius: 10px;
+  border: none;
+  padding: 10px 20px;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  &:hover {
+    color: #eca400;
+  }
+
+  &:active {
+    color: #eca400;
+    opacity: 0.9;
+  }
+`;
+
+export const FormAddAddress = styled.form`
+  width: 500px;
+  background-color: #141414;
+  padding: 30px;
+  margin-top: 40px;
+  border: 3px solid black;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  border: 3px solid #fff;
+`;
+
+export const TitleForm = styled.h1`
+  font-family: var(--font-emphasis);
+  font-size: 3rem;
+  color: var(--color-white);
+  text-align: center;
+  text-transform: uppercase;
+  line-height: 88.896%;
+`;
+
+export const LabelForm = styled.label`
+  font-family: var(--font-emphasis);
+  font-size: 1.6rem;
+  color: var(--color-white);
+  margin: 0 auto;
+  text-transform: uppercase;
+`;
+
+export const InputFormAdd = styled.input`
+  width: 90%;
+  height: 30px;
+  font-family: var(--font-text);
+  font-size: 2rem;
+  display: block;
+  outline: none;
+  padding-left: 10px;
+`;
+
+export const ButtonAddAddress = styled.input`
+  width: 40%;
+  font-size: 3rem;
+  font-family: var(--font-text);
+  color: var(--color-white);
+  background-color: var(--color-purple);
+  padding: 2px;
+  border-radius: 14px;
+  text-transform: uppercase;
+  margin: 0 auto;
+  border: none;
+  cursor: pointer;
+`;
+
+export const ButtonHiddenForm = styled.button`
+  width: 250px;
+  font-size: 2rem;
+  font-family: var(--font-text);
+  color: var(--color-white);
+  background-color: var(--color-purple);
+  padding: 2px;
+  border-radius: 14px;
+  text-transform: uppercase;
+  cursor: pointer;
+  border: none;
+  margin-top: 20px;
+`;
