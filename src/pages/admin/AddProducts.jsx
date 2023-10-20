16ff4731ae801871b67ca22968532fbd0c0c8846
AlexMoreni <alex.moreni47@gmail.com>
2023-10-20 16:28:06 -0300

    Adding function of adding new products (admin)

--- src/pages/admin/AddProducts.jsx
+++ src/pages/admin/AddProducts.jsx
@@ -14,11 +14,25 @@ import {
   ImgCardProduct,
   NameCardProduct,
   EmphasisName,
+  FormAddProduct,
+  LabelFormAddProduct,
+  InputFormAddProduct,
+  TextAreaFormAddProduct,
+  SelectFormAddProduct,
+  ButtonFormAddProduct,
+  ButtonCloseFormAddProduct,
+  BackgroundFormAddProduct,
 } from "./AddProducts.style";
 
 const AddProducts = () => {
   const [products, setProducts] = useState(false);
   const [messageError, setMessageError] = useState("");
+  const [formAdd, setFormAdd] = useState(false);
+  const [nameProduct, setNameProduct] = useState("");
+  const [descriptionProduct, setDescriptionProduct] = useState("");
+  const [valueProduct, setValueProduct] = useState("");
+  const [categoryProduct, setCategoryProduct] = useState("");
+  const [imgProduct, setImgProduct] = useState();
 
   useEffect(() => {
     axios
@@ -38,7 +52,7 @@ const AddProducts = () => {
           );
         }
       });
-  }, []);
+  }, [formAdd]);
 
   const handleSearch = async (e) => {
     e.preventDefault();
@@ -61,10 +75,42 @@ const AddProducts = () => {
     }
   };
 
+  const handleBackground = (value) => {
+    window.scrollTo(0, 0);
+
+    if (value === "ativar") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  };
+
+  const handleNewProduct = (e) => {
+    e.preventDefault();
+
+    axios.defaults.withCredentials = true;
+
+    axios
+      .post("http://localhost:3000/burguer/newproduct", {
+        name: nameProduct,
+        description: descriptionProduct,
+        value: valueProduct,
+        category: categoryProduct,
+        img: imgProduct,
+      })
+      .then((response) => {
+        if (response.data.message === "Produto cadastrado!") {
+          setFormAdd(false);
+          handleBackground();
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   console.log(products);
 
   return (
-    <Container>
+    <Container id="container">
       <FieldSearch onSubmit={handleSearch}>
         <Input
           type="text"
@@ -76,7 +122,14 @@ const AddProducts = () => {
       </FieldSearch>
       <ContainerTitle>
         <Title>Comidas e bebidas</Title>
-        <ButtonAdd>+</ButtonAdd>
+        <ButtonAdd
+          onClick={() => {
+            handleBackground("ativar");
+            setFormAdd(true);
+          }}
+        >
+          +
+        </ButtonAdd>
       </ContainerTitle>
       {messageError && <p>{messageError}</p>}
       <ContainerCardsProducts>
@@ -102,6 +155,79 @@ const AddProducts = () => {
             </CardProduct>
           ))}
       </ContainerCardsProducts>
+      {formAdd && (
+        <>
+          <BackgroundFormAddProduct
+            onClick={() => {
+              setFormAdd(false);
+              handleBackground();
+            }}
+          ></BackgroundFormAddProduct>
+          <FormAddProduct onSubmit={handleNewProduct}>
+            <LabelFormAddProduct>
+              Nome
+              <InputFormAddProduct
+                type="text"
+                placeholder="Digite o nome do produto"
+                name="name"
+                onChange={(e) => setNameProduct(e.target.value)}
+              />
+            </LabelFormAddProduct>
+            <LabelFormAddProduct>
+              Descrição
+              <TextAreaFormAddProduct
+                name="description"
+                placeholder="Digite a descrição do produto"
+                cols="30"
+                rows="3"
+                onChange={(e) => setDescriptionProduct(e.target.value)}
+              ></TextAreaFormAddProduct>
+            </LabelFormAddProduct>
+            <LabelFormAddProduct>
+              Valor
+              <InputFormAddProduct
+                type="number"
+                placeholder="Digite o valor do produto"
+                name="value"
+                onChange={(e) => setValueProduct(e.target.value)}
+              />
+            </LabelFormAddProduct>
+            <LabelFormAddProduct>
+              Categoria:
+              <SelectFormAddProduct
+                name="category"
+                id="category"
+                onChange={(e) => setCategoryProduct(e.target.value)}
+              >
+                <option value="" hidden></option>
+                <option value="hambúrgueres">Hambúrguer</option>
+                <option value="acompanhamento">Acompanhamento</option>
+                <option value="bebida">Bebida</option>
+                <option value="sobremesa">Sobremesa</option>
+                <option value="salada">Salada</option>
+              </SelectFormAddProduct>
+            </LabelFormAddProduct>
+            <LabelFormAddProduct>
+              URL
+              <InputFormAddProduct
+                type="text"
+                placeholder="URL da imagem"
+                name="img"
+                onChange={(e) => setImgProduct(e.target.value)}
+              />
+            </LabelFormAddProduct>
+            <ButtonFormAddProduct>Cadastrar</ButtonFormAddProduct>
+            <ButtonCloseFormAddProduct
+              onClick={() => {
+                setFormAdd(false);
+                handleBackground();
+              }}
+            >
+              x
+            </ButtonCloseFormAddProduct>
+          </FormAddProduct>
+        </>
+      )}
     </Container>
   );
 };
